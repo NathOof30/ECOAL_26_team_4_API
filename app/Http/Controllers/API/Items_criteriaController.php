39cae5335ec902d5b97bf5d615879ad9b29a8952
Keskidy <matheomollet7@gmail.com>
2026-03-09 15:47:44 +0100

    installation of API, models and controllers in the folder API

---
+++ app/Http/Controllers/API/Items_criteriaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Items_criteria;
+use Illuminate\Http\Request;
+
+class Items_criteriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Items_criteria $items_criteria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Items_criteria $items_criteria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Items_criteria $items_criteria)
+    {
+        //
+    }
+}
