2b540c68721e09dc6689b57f84e313e082bd4e7c
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2026-03-10 14:35:08 +0100

    Register 'user_type' middleware alias
    Import EnsureUserType and register a 'user_type' middleware alias in bootstrap/app.php via the withMiddleware closure. This adds an alias for App\Http\Middleware\EnsureUserType so it can be referenced by name in routes and middleware configurations.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureUserType;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'user_type' => EnsureUserType::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
